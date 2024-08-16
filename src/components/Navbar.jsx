8a54fb7d20851f543e719f04827abce9cad6f6c7
Andres Castro <andrescn20@gmail.com>
2024-08-16 01:19:27 -0600

    Recreate Navbar with Tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,30 +16,26 @@ const Navbar = () => {
   }
 
   return (
-    <div className="navbar bg-base-100 shadow-md">
-      <div className="flex-none">
-      </div>
-      <div className="flex-1 mx-4">
-        <label htmlFor="my-drawer" className="drawer-button">
+    <div className="flex py-4 shadow-lg px-4 mb-2">
+      <div className="flex-grow space-x-8 items-center justify-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            className="inline-block h-5 w-5 stroke-current">
+            className="inline-block h-5 w-5 stroke-current align-middle mb-1">
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
               d="M4 6h16M4 12h16M4 18h16"></path>
           </svg>
-        </label>
-        <button value="inicio" onClick = { (e) => navBarClick(e)}className="btn btn-ghost text-xl">Inicio</button>
-        <button value="dispositivo" onClick = { (e) => navBarClick(e)}className="btn btn-ghost text-xl" href="">Agregar Dispositivo</button>
-        <button value="general" onClick = { (e) => navBarClick(e)}className="btn btn-ghost text-xl">Panel General</button>
-        <button value="ayuda" onClick = { (e) => navBarClick(e)}className="btn btn-ghost text-xl">Ayuda</button>
+        <button value="inicio" onClick = { (e) => navBarClick(e)}className="text-xl">Inicio</button>
+        <button value="dispositivo" onClick = { (e) => navBarClick(e)}className="text-xl" href="">Agregar Dispositivo</button>
+        <button value="general" onClick = { (e) => navBarClick(e)}className="text-xl">Panel General</button>
+        <button value="ayuda" onClick = { (e) => navBarClick(e)}className="text-xl">Ayuda</button>
       </div>
-      <div className="flex-none">
-        <button className="btn btn-square btn-ghost">
+      <div className="">
+        <button className="">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
