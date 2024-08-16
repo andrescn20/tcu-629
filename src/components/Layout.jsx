716bdae36118866ed3e110af61accc74efffc294
Andres Castro <andrescn20@gmail.com>
2024-08-16 01:19:39 -0600

    Remove Daisy code and drawer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,20 +4,12 @@ import Footer from "./Footer";
 
 const Layout = ({ children }) => {
     return (
-        <div className="drawer h-full">
-            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col">
-                <Navbar />
+        <div className="h-full flex flex-col">
+            <Navbar />
+            <div className="flex-grow">
                 {children}
-                <Footer />
             </div>
-            <div className="drawer-side">
-                <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-                <ul className="menu bg-white/80 rounded-r-xl text-base-content min-h-full w-80 p-4">
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
-                </ul>
-            </div>
+            <Footer />
         </div>
     );
 }
