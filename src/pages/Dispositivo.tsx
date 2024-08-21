38542533c882c64ec49e4e6fb39f7a3fb86c593e
Andres Castro <andrescn20@gmail.com>
2024-08-20 23:17:54 -0600

    Create Dispositivo.tsx

---
+++ src/pages/Dispositivo.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import Layout from "../components/Layout";
+import Device from "../components/Device";
+import { IDevice } from "../components/interfaces";
+import { useSearchParams } from "react-router-dom";
+
+const Dispositivo = () => {
+
+    const [queryParameters] = useSearchParams();
+    const deviceId = queryParameters.get('deviceId');
+
+  const [devices, setDevices] = useState<IDevice[]>([]);
+
+  const url = import.meta.env.VITE_API_URL;
+
+  const fetchData = async () => {
+    const response = await fetch(url + "/Device/GetAllDevices");
+    console.log(response);
+    const devices = await response.json();
+    console.log(devices);
+    setDevices(devices);
+  };
+
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <Layout>
+      <div className="flex justify-center">DIspositivo Id: {deviceId}</div>
+    </Layout>
+  );
+};
+
+export default Dispositivo;
