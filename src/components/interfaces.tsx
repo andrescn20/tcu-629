5da2745e3043d1bfa16a417421f426a305abb44c
Andres Castro <andrescn20@gmail.com>
2024-08-23 08:18:20 -0600

    Refactor Interfaces

--- src/components/interfaces.tsx
+++ src/components/interfaces.tsx
@@ -1,18 +1,48 @@
+export interface INewDevice {
+  deviceTypeId: number;
+  description: string;
+  location: string;
+  sensors: number[];
+  boardId: number;
+}
+
 export interface IDevice {
-    deviceId: number;
-    deviceTypeId: number;
-    description: string;
-    location: string;
-    added_at: Date;
-    deviceType: string;
+  deviceId: number;
+  deviceTypeId: number;
+  description: string;
+  location: string;
+  added_at: Date;
+  deviceType: string;
+  sensors: ISensor[];
+  board: IBoard;
+}
+export interface IBoard {
+  boardId: number;
+  microcontroller: string;
+  description: string;
+  isInstalled: boolean;
+}
+
+export interface ISensor {
+  sensorId: number;
+  sensorTypeId: number;
+  description: string;
+  sensorName: string;
+  sensorType: string;
+  isAvailable: boolean;
 }
 
 export interface IDeviceType {
-    typeId: number;
-    type: string;
+  typeId: number;
+  type: string;
 }
 
 export interface ISensorType {
-    sensorTypeId: number;
-    type: string;
+  sensorTypeId: number;
+  type: string;
+}
+
+export interface IDDOptions {
+  key: number;
+  text: string;
 }
