a5d3d180b69058ec1b9711c6244953f9913f2046
Andres Castro <andrescn20@gmail.com>
2024-08-23 08:16:33 -0600

    Solve npm package issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,9 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "home": "url('/src/assets/fondo_Hogar.png')",
-      },
-    },
+  ],  theme: {
+    extend: {},
   },
+  plugins: [],
 }
+
