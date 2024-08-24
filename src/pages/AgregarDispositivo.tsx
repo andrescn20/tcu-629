456c96767c1cfa20aa2c92a23a05dc15ef7a02da
Andres Castro <andrescn20@gmail.com>
2024-08-23 20:30:53 -0600

    Updated Logic, import error and validation for cases with no sensors or boards

--- src/pages/AgregarDispositivo.tsx
+++ src/pages/AgregarDispositivo.tsx
@@ -4,7 +4,7 @@ import { Dropdown } from "@fluentui/react/lib/Dropdown";
 import { TextField } from "@fluentui/react/lib/TextField";
 import { PrimaryButton } from "@fluentui/react";
 import { useNavigate } from "react-router-dom";
-import { IDevice, INewDevice } from "../components/Interfaces";
+import { IDevice, INewDevice, IType } from "../components/Interfaces";
 import SensorFormField from "../components/SensorFormField";
 import BoardFormField from "../components/BoardFormField";
 
@@ -15,11 +15,6 @@ const AgregarDispositivo = () => {
   };
 
   const navigate = useNavigate();
-
-  interface IType {
-    typeId: number;
-    type: string;
-  }
 
   interface ITypeOptions {
     key: number;
@@ -36,17 +31,17 @@ const AgregarDispositivo = () => {
     deviceTypeId: 0,
     location: "",
     description: "",
-    sensors: [],
+    sensorIds: [],
     boardId: 0,
   });
-  const [sensorFields, setSensorFields] = useState<JSX.Element[]>([]);
 
   const hasDuplicates = (selectedIds: number[]) => {
     return selectedIds.some((item: number, index: number) => selectedIds.indexOf(item) !== index);
   };
 
   const createDevice = async () => {
-    if (newDevice.sensors.length === 0) {
+    let createdDeviceId = 0
+    if (newDevice.sensorIds.length === 0) {
       alert("Debe seleccionar al menos un sensor");
       throw new Error();
     }
@@ -66,7 +61,7 @@ const AgregarDispositivo = () => {
       alert("Debe ingresar una descripción");
       throw new Error();
     }
-    if (newDevice.sensors.includes(0)) {
+    if (newDevice.sensorIds.includes(0)) {
       alert("Debe rellenar todos los campso de los sensores o eliminar los espacios vacíos");
       throw new Error();
     }
@@ -75,29 +70,43 @@ const AgregarDispositivo = () => {
       throw new Error();
     }
     console.log("Creating Device");
-    const response = await fetch(url + "/Hardware/CreateNewDevice", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newDevice),
-    });
-    const device: IDevice = await response.json();
-    return device;
+    try {
+      const response = await fetch(url + "/Device/CreateNewDevice", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newDevice),
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(data);
+      const device: IDevice = await response.json();
+      if(device.deviceId !== null) {
+      createdDeviceId = device.deviceId;
+      }
+      createdDeviceId = 0;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+    return createdDeviceId;
   };
-  const handleSubmit = async (e) => {
+
+const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault();
     try {
-      const device = await createDevice();
-      alert(`Dispositvo creado con éxito 
-                Id Dispositivo: ${device.deviceId}`);
-      navigate("/home");
+        const deviceId: number = await createDevice();
+        alert(`Dispositvo creado con éxito 
+                            Id Dispositivo: ${deviceId}`);
+        navigate("/home");
     } catch (error) {
-      alert("ATENCION: Error al crear el dispositivo");
-      console.error(error);
-      navigate("/agregardispositivo");
+        alert("ATENCION: Error al crear el dispositivo");
+        console.error(error);
+        navigate("/agregardispositivo");
     }
-  };
+};
 
   const generateDeviceTypeOptions = (deviceTypes: IType[]) => {
     if (deviceTypes.length === 0) return [];
@@ -108,25 +117,25 @@ const AgregarDispositivo = () => {
   };
 
   const fetchDeviceTypes = async () => {
-    const response = await fetch(url + "/Hardware/GetDeviceTypes?withDevices=false");
+    const response = await fetch(url + "/Device/GetDeviceTypes?withDevices=false");
     const data = await response.json();
     setDeviceTypes(data);
     generateDeviceTypeOptions(data);
   };
   const fetchSensorTypes = async () => {
-    const response = await fetch(url + "/Hardware/GetSensorTypes");
+    const response = await fetch(url + "/Sensor/GetSensorTypes");
     const data = await response.json();
     setSensorTypes(data);
   };
 
   const fetchSensors = async () => {
-    const response = await fetch(url + "/Hardware/GetAllSensors");
+    const response = await fetch(url + "/Sensor/GetAllSensors");
     const data = await response.json();
     setSensors(data);
   };
 
   const fetchBoards = async () => {
-    const response = await fetch(url + "/Hardware/GetAllBoards");
+    const response = await fetch(url + "/Board/GetAllBoards");
     const data = await response.json();
     setBoards(data);
   };
@@ -139,7 +148,7 @@ const AgregarDispositivo = () => {
   }, []);
 
   useEffect(() => {
-    setNewDevice({ ...newDevice, sensors: selectedSensors });
+    setNewDevice({ ...newDevice, sensorIds: selectedSensors });
   }, [selectedSensors]);
 
   useEffect(() => {
@@ -153,40 +162,55 @@ const AgregarDispositivo = () => {
   return (
     <Layout>
       <div className="flex flex-col h-full items-center pt-4 space-y-4">
-        <h2 className="text-2xl">Nuevo Dispositivo</h2>
-        <form onSubmit={handleSubmit} className="w-1/2">
-          <p>Dispositivo</p>
-          <Dropdown
-            required={true}
-            placeholder="Seleccione una opción"
-            label="Tipo de Dispositivo"
-            options={deviceTypeOptions}
-            styles={dropdownStyles}
-            onChange={(e, item) => {
-              setNewDevice({ ...newDevice, deviceTypeId: item.key });
-            }}
-          />
-          <TextField
-            label="Ubicación"
-            placeholder="Pabellón Central"
-            required
-            value={newDevice.location}
-            onChange={(e) => setNewDevice({ ...newDevice, location: e.target.value })}
-          />
-          <TextField
-            label="Descripción"
-            placeholder="Calentador para las habitaciones 1 al 4"
-            value={newDevice.description}
-            onChange={(e) => setNewDevice({ ...newDevice, description: e.target.value })}
-          />
-          <p>Sensores</p>
-          <SensorFormField sensors={sensors} setSensor={setSelectedSensors} currentSelection={newDevice.sensors} />
-          <p>Placa</p>
-          <BoardFormField boards={boards} setBoardId={setSelectedBoardId} />
-          <div className="flex justify-start">
-            <PrimaryButton className="mt-4 mr-4" text="Guardar" onClick={() => createDevice()} />
-          </div>
-        </form>
+        {sensors.length === 0 || boards.length === 0 ? (
+          <>
+            <h2>No hay sensores o dispositivos disponibles.</h2>
+            <a href="./configuracion">Ir a Configuración</a>
+          </>
+        ) : (
+          <>
+            <h2 className="text-2xl">Nuevo Dispositivo</h2>
+            <form onSubmit={handleSubmit} className="w-1/2">
+              <p className="font-bold my-4">Dispositivo</p>
+              <Dropdown
+                required={true}
+                placeholder="Seleccione una opción"
+                label="Tipo de Dispositivo"
+                options={deviceTypeOptions}
+                styles={dropdownStyles}
+                onChange={(e, item) => {
+                  if (item) {
+                    setNewDevice({ ...newDevice, deviceTypeId: Number(item.key) });
+                  }
+                }}
+              />
+              <TextField
+                label="Ubicación"
+                placeholder="Pabellón Central"
+                required
+                value={newDevice.location}
+                onChange={(e) => setNewDevice({ ...newDevice, location: (e.target as HTMLInputElement).value })}
+              />
+              <TextField
+                label="Descripción"
+                placeholder="Calentador para las habitaciones 1 al 4"
+                value={newDevice.description}
+                onChange={(e) => setNewDevice({ ...newDevice, description: (e.target as HTMLInputElement).value })}
+              />
+              <p className="font-bold my-4">Sensores</p>
+              <SensorFormField
+                sensors={sensors}
+                setSensor={setSelectedSensors}
+                currentSelection={newDevice.sensorIds}
+              />
+              <p className="font-bold my-4">Controlador</p>
+              <BoardFormField boards={boards} setBoardId={setSelectedBoardId} />
+              <div className="flex justify-start">
+                <PrimaryButton className="mt-4 mr-4" text="Guardar" type="submit" />
+              </div>
+            </form>
+          </>
+        )}
       </div>
     </Layout>
   );
