b5f7af35195af6f21a90a4e7dc6787ee5e0a7995
Andrés Castro <andrescn20@gmail.com>
2024-09-07 14:33:57 -0600

    Update Login page with new login and signup forms

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,36 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import LoginButton from '../components/LoginButton';
-import LogoutButton from '../components/LogoutButton';
-import Profile from './Profile';
-import { useAuth0 } from '@auth0/auth0-react';
-import { PrimaryButton } from '@fluentui/react/lib/Button';
+import { DefaultButton, PrimaryButton } from '@fluentui/react/lib/Button';
+import LoginForm from '../components/LoginForm';
+import SignupForm from '../components/SignupForm';
 
 const Login = () => {
+  const [login, setLogin] = useState(true);
   const navigate = useNavigate();
 
-  const {isAuthenticated} = useAuth0();
+  const handleSignUp = async () => {
+    setLogin(true);
+  };
 
-  useEffect(() => {
-    if (isAuthenticated) {
+  const handleLogin = (token) => {
+    if (token) {
       navigate('/home');
     }
-  }, []);
-
-  const handleSignUp = async () => {
-    navigate('/home');
   };
-
-  const handleLogin = async () => {
-      navigate('/home');
-  };
-
   return (
-    <div className='h-full flex flex-col items-center justify-center'>
-      <h1 >App Monitoreo TCU 629</h1>
-      <div className='space-x-5 my-4'>
-        {/* <LoginButton /> */}
-        <PrimaryButton text="Log In" onClick={() => handleLogin()} />
+    <div className='flex flex-col m-12'>
+      <div>
+        <h1 className='text-2xl'>TCU 629</h1>
+        <h1 className='text-xl'>Aplicación Monitoreo</h1>
+      </div>
+      <div className='grow my-12 w-full flex flex-col items-center justify-center'>
+        <div className='space-x-5 my-4 w-1/3'>
+          {login && <LoginForm onLogin = {handleLogin}/>}
+          {!login && <SignupForm onSignUp = {handleSignUp}/>}
+        </div>
+        <DefaultButton text={login ? "Usuario Nuevo" : "Usuario Registrado"} onClick={() => setLogin(!login)} />
       </div>
     </div>
   );
