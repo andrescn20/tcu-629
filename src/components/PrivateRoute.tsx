8e57e5f22f8dafd89760107b919bd63d9a95ea6c
Andrés Castro <andrescn20@gmail.com>
2024-09-08 00:24:16 -0600

    Se agrega restricción por rol a las vistas

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,34 +1,35 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { jwtDecode } from "jwt-decode";
-interface DecodedToken {
-  exp: number;
-}
+import { decodeToken} from '../utils/decodeToken';
 
-interface PrivateRouteProps {
-  children: React.ReactElement; 
-}
+  interface PrivateRouteProps {
+    children: React.ReactElement;
+    requiredRole?: string; // Specify required role
+  }
 
-const PrivateRoute = ({children}:PrivateRouteProps) => {
-  const token = localStorage.getItem('token');
+const PrivateRoute = ({children , requiredRole}:PrivateRouteProps) => {
 
-  if (token) {
     try {
-      const decodedToken: DecodedToken = jwtDecode(token);
       const currentTime = Date.now() / 1000;
-
+      const decodedToken = decodeToken();
+    
       if (decodedToken.exp < currentTime) {
         localStorage.removeItem('token');
         return <Navigate to="/login" replace />;
       }
+      
+      const role = decodedToken.role
+
+      if (requiredRole && role != requiredRole) {
+        return <Navigate to="/unauthorized" replace />;
+      }
 
       return children;
+      
     } catch (error) {
       localStorage.removeItem('token');
       return <Navigate to="/login" replace />;
     }
-  }
-  return <Navigate to="/login" replace />;
 };
 
 export default PrivateRoute;
