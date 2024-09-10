adddd7fbc22b3c8de5ca79bec69e237dbdd57764
Andrés Castro <andrescn20@gmail.com>
2024-09-09 18:45:06 -0600

    Reestructurar componentes para mostrar correctamente la información

--- src/components/Device.tsx
+++ src/components/Device.tsx
@@ -1,37 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DeviceCard from "../components/DeviceCard";
-import { IDevice } from "../utils/Interfaces";
+import { IDevice, IDeviceStats } from "../utils/Interfaces";
 import { PrimaryButton } from "../../node_modules/@fluentui/react/lib/Button";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import fetchWithAuth from "../utils/fetchWithAuth";
 
 interface DeviceProps {
   device: IDevice;
 }
 
 const Device = ({ device }: DeviceProps) => {
-    
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  
+  const [stats, setStats] = useState<IDeviceStats>();
+
+  const fetchData = async () => {
+    const id = device.deviceId.toString();
+    const response = await fetchWithAuth(`/Device/GetDeviceStats?deviceId=${id}`);
+    const stats = await response.json();
+    console.log(stats);
+    setStats(stats);
+  };
+
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
-    <div className=" rounded-md px-8 py-4 mb-4">
-      <div className="grid grid-cols-3 mb-2">
-          <p className="col-start-1">
-            <strong>Ubicación:</strong> {device.location}
-          </p>
-          <p className="" key={device.deviceId}>
-            <strong>Tipo:</strong> {device.deviceType}
-          </p>
-          <p className="col-start-2 row-start-1 col-span-2">
-            <strong>Descripción:</strong> {device.description}
-          </p>
+    <div className="rounded-md px-8 py-4 mb-4">
+      <div className="grid grid-cols-3 mb-2 gap-2">
+        <p className="col-start-1 row-start-2">
+          <strong>Ubicación:</strong> {device.location}
+        </p>
+        <p className="" key={device.deviceId}>
+          <strong>Tipo:</strong> {device.deviceType}
+        </p>
+        <p className="col-start-2 row-start-1 col-span-2">
+          <strong>Descripción:</strong> {device.description}
+        </p>
+      <div className="row-start-2 col-start-3 col-span-1 flex justify-end mr-2">
+        <PrimaryButton  onClick={() => navigate(`/dispositivo?deviceId=${device.deviceId}&description=${device.description}&location=${device.location}&deviceType=${device.deviceType}`)}>Ver Detalles</PrimaryButton>
+        </div>
       </div>
-      <PrimaryButton onClick={() => navigate(`/dispositivo?deviceId=${device.deviceId}`)}>Ver Detalles</PrimaryButton>
-      <div className="grid grid-cols-2">
-        <DeviceCard title="Temperatura Actual" value="25°C" />
-        <DeviceCard title="Temperatura Promedio" value="25°C" />
-        <DeviceCard title="Temperatura Maxima" value="25°C" />
-        <DeviceCard title="Temperatura Minima" value="25°C" />
-      </div>
+      {stats && <div className="grid grid-cols-2">
+        <DeviceCard title="Temperatura Mas Reciente" value={stats.latestTemperature} date={stats.latestTemperatureTime} />
+        <DeviceCard title="Temperatura Promedio" value={stats.medianTemperature}  date={undefined}/>
+        <DeviceCard title="Temperatura Maxima" value={stats.maxTemperature} date={stats.maxTemperatureTime} />
+        <DeviceCard title="Temperatura Minima" value={stats.minTemperature} date={stats.minTemperatureTime} />
+      </div>}
     </div>
   );
 };
--- src/components/DeviceCard.tsx
+++ src/components/DeviceCard.tsx
@@ -1,37 +1,43 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
-  
+import { useNavigate } from "react-router-dom";
 
 interface DeviceCardProps {
-    title: string;
-    value: string;
+  title: string;
+  value: number;
+  date: string | undefined;
 }
 
-  const DeviceCard = ({ title, value }: DeviceCardProps) => {
-    return (
-      <div className="flex flex-col p-4 space-y-8 m-2 bg-white border-slate-500 border-2 rounded-md">
-        <div className="flex justify-between items-center">
-          <p className="text-xl font-bold">{title}</p>
-          <svg width="28" height="28" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clip-path="url(#clip0_6_296)">
-              <path
-                d="M28 29.52V7C28 5.67392 27.4732 4.40215 26.5355 3.46447C25.5979 2.52678 24.3261 2 23 2C21.6739 2 20.4021 2.52678 19.4645 3.46447C18.5268 4.40215 18 5.67392 18 7V29.52C16.3946 30.5927 15.1766 32.1532 14.5261 33.9712C13.8756 35.7891 13.827 37.7681 14.3875 39.6158C14.948 41.4635 16.0878 43.082 17.6387 44.2322C19.1895 45.3824 21.0692 46.0033 23 46.0033C24.9308 46.0033 26.8105 45.3824 28.3613 44.2322C29.9122 43.082 31.052 41.4635 31.6125 39.6158C32.173 37.7681 32.1244 35.7891 31.4739 33.9712C30.8234 32.1532 29.6054 30.5927 28 29.52Z"
-                stroke="#1E1E1E"
-                stroke-width="4"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </g>
-            <defs>
-              <clipPath id="clip0_6_296">
-                <rect width="48" height="48" fill="white" />
-              </clipPath>
-            </defs>
-          </svg>
-        </div>
-        <div>{value}</div>
+const formatDateToLocalTime = (utcDateString: string): string => {
+  const utcDate = new Date(utcDateString);
+  return utcDate.toLocaleString(); // Converts to local time and formats it
+};
+
+const DeviceCard = ({ title, value, date }: DeviceCardProps) => {
+  return (
+    <div className="flex flex-col p-4 m-2 bg-white border-slate-500 border-2 rounded-md">
+      <div className="flex justify-between items-center">
+        <p className="text-md font-bold">{title}:</p>
+        <svg width="28" height="28" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g clip-path="url(#clip0_6_296)">
+            <path
+              d="M28 29.52V7C28 5.67392 27.4732 4.40215 26.5355 3.46447C25.5979 2.52678 24.3261 2 23 2C21.6739 2 20.4021 2.52678 19.4645 3.46447C18.5268 4.40215 18 5.67392 18 7V29.52C16.3946 30.5927 15.1766 32.1532 14.5261 33.9712C13.8756 35.7891 13.827 37.7681 14.3875 39.6158C14.948 41.4635 16.0878 43.082 17.6387 44.2322C19.1895 45.3824 21.0692 46.0033 23 46.0033C24.9308 46.0033 26.8105 45.3824 28.3613 44.2322C29.9122 43.082 31.052 41.4635 31.6125 39.6158C32.173 37.7681 32.1244 35.7891 31.4739 33.9712C30.8234 32.1532 29.6054 30.5927 28 29.52Z"
+              stroke="#1E1E1E"
+              stroke-width="4"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </g>
+          <defs>
+            <clipPath id="clip0_6_296">
+              <rect width="48" height="48" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
       </div>
-    );
-  };
+      <div className="text-4xl font-bold flex justify-center my-2">{value} °C</div>
+      {date && <div>Tomada el: {formatDateToLocalTime(date)}</div>}
+    </div>
+  );
+};
 
 export default DeviceCard;
