683fff2dcf3d2b9a5de8d53458b6331eae72cdc8
Andrés Castro <andrescn20@gmail.com>
2024-10-02 21:57:42 -0600

    Eliminar console.log

--- src/utils/formatDate.tsx
+++ src/utils/formatDate.tsx
@@ -1,6 +1,5 @@
 const formatDateToLocalTime = (utcDateString: string, invert:boolean = false): string => {
   const utcDate = new Date(utcDateString);
-  console.log("original", utcDate);
   let localdate;
   if(invert){
     localdate = new Date(utcDate.getTime() - utcDate.getTimezoneOffset()*60*1000);
